6befb7f4b6ff190325bd23f13fd7f0546aaddb01
Zain Javaid <zainmfj@gmail.com>
2023-12-28 23:43:39 -0600

    Added user authentication skeleton

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 
 
 load_dotenv('secret.env')
@@ -12,9 +12,29 @@ app.secret_key = os.environ['FLASK_SECRET_KEY']
 def index():
     return render_template('index.html')
 
+@app.route('/portal/login', methods=['GET', 'POST'])
+def portal_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        session['is_logged_in'] = True
+        print('here')
+        return redirect('/portal')
+    else:
+        return render_template('login.html')
+
 @app.route('/portal')
 def portal():
-    return render_template('cms_portal.html')
+    if session.get('is_logged_in'):
+        return render_template('cms_portal.html')
+    else:
+        print('redirecting back')
+        return redirect('/portal/login')
+
+@app.route('/portal/logout')
+def portal_logout():
+    session['is_logged_in'] = False
+    return redirect('/portal')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
