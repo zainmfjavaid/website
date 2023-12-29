2b793af12a5ddfc4ded8732ebcdb8b0bf212430d
Zain Javaid <zainmfj@gmail.com>
2023-12-28 23:21:29 -0600

    Added portal route + config file + requirements file

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
 
+load_dotenv('secret.env')
+
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET_KEY']
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/portal')
+def portal():
+    return render_template('cms_portal.html')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
